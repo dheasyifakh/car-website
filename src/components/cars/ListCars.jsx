2f1ab7ccfc04ba67942d417d3c3dcd2c429383b4
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-03-26 09:51:27 +0700

    Styling Filter

--- src/components/cars/ListCars.jsx
+++ src/components/cars/ListCars.jsx
@@ -34,109 +34,19 @@ const ListCars = (carData) => {
             <div className="mt-4 lg:mt-8 lg:grid lg:grid-cols-4 lg:items-start lg:gap-8">
                 <div className="hidden space-y-4 lg:block">
                     <div>
-                        <label for="SortBy" className="block text-xs font-medium text-gray-700"> Sort By </label>
+                        <label for="SortBy" className="block text-xs font-medium text-gray-700">Select a Car Type:</label>
 
-                        <select id="SortBy" className="mt-1 rounded border-gray-300 text-sm">
-                            <option>Sort By</option>
-                            <option value="Title, DESC">Title, DESC</option>
-                            <option value="Title, ASC">Title, ASC</option>
-                            <option value="Price, DESC">Price, DESC</option>
-                            <option value="Price, ASC">Price, ASC</option>
+                        <select id="SortBy" value={selectedCarType} onChange={handleCarTypeChange} className="mt-1 rounded border-gray-300 text-sm">
+                            <option value="">All Car</option>
+                            <option value="electrical">Electrical Car</option>
+                            <option value="twowheels">Two Wheels Car</option>
+                            <option value="sport">Sport Car</option>
                         </select>
                     </div>
                 <div>
-                <p className="block text-xs font-medium text-gray-700">Filters</p>
-                <div className="mt-1 space-y-2">
-                <h1>Select a Car Type:</h1>
-                <select value={selectedCarType} onChange={handleCarTypeChange}>
-                    <option value="">Select</option>
-                    <option value="electrical">Electrical Car</option>
-                    <option value="twowheels">Two Wheels Car</option>
-                    <option value="sport">Sport Car</option>
-                </select>
+        
                 </div>
-                <div className="mt-1 space-y-2">
-                    <details
-                    className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden"
-                    >
-                    <summary
-                        className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
-                    >
-                        <span className="text-sm font-medium"> Availability </span>
-
-                        <span className="transition group-open:-rotate-180">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            className="h-4 w-4"
-                        >
-                            <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                            />
-                        </svg>
-                        </span>
-                    </summary>
-
-                    <div className="border-t border-gray-200 bg-white">
-                        <header className="flex items-center justify-between p-4">
-                        <span className="text-sm text-gray-700"> 0 Selected </span>
-
-                        <button type="button" className="text-sm text-gray-900 underline underline-offset-4">
-                            Reset
-                        </button>
-                        </header>
-
-                        <ul className="space-y-1 border-t border-gray-200 p-4">
-                        <li>
-                            <label for="FilterInStock" className="inline-flex items-center gap-2">
-                            <input
-                                type="checkbox"
-                                id="FilterInStock"
-                                className="size-5 rounded border-gray-300"
-                            />
-
-                            <span className="text-sm font-medium text-gray-700"> In Stock (5+) </span>
-                            </label>
-                        </li>
-
-                        <li>
-                            <label for="FilterPreOrder" className="inline-flex items-center gap-2">
-                            <input
-                                type="checkbox"
-                                id="FilterPreOrder"
-                                className="size-5 rounded border-gray-300"
-                            />
-
-                            <span className="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
-                            </label>
-                        </li>
-
-                        <li>
-                            <label for="FilterOutOfStock" className="inline-flex items-center gap-2">
-                            <input
-                                type="checkbox"
-                                id="FilterOutOfStock"
-                                className="size-5 rounded border-gray-300"
-                            />
-
-                            <span className="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
-                            </label>
-                        </li>
-                        </ul>
-                    </div>
-                    </details>
-
-                  
-                   
-
-                    
-                    </div>
-                </div>
+            
             </div>
 
             <div className="lg:col-span-3">
