747553fa148d094e84ffd181bb803b3859fff568
Dhea Syifa Khoirunnisa <dheasyifa.khoirunnisa@spentera.com>
2024-03-26 14:54:43 +0700

    Remove typo

--- src/components/cars/ListCars.jsx
+++ src/components/cars/ListCars.jsx
@@ -30,61 +30,50 @@ const ListCars = (carData) => {
                     </svg>
                 </button>
             </div>
-
             <div className="mt-4 lg:mt-8 lg:grid lg:grid-cols-4 lg:items-start lg:gap-8">
                 <div className="hidden space-y-4 lg:block">
                     <div>
                         <label for="SortBy" className="block text-xs font-medium text-gray-700">Select a Car Type:</label>
-
                         <select id="SortBy" value={selectedCarType} onChange={handleCarTypeChange} className="mt-1 rounded border-gray-300 text-sm">
                             <option value="">All Car</option>
                             <option value="electrical">Electrical Car</option>
-                            <option value="twowheels">Two Wheels Car</option>
+                            <option value="twowheels">Two Wheels</option>
                             <option value="sport">Sport Car</option>
                         </select>
                     </div>
                 <div>
-        
-                </div>
-            
             </div>
-
-            <div className="lg:col-span-3">
-                
-                <ul className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        </div>
+        <div className="lg:col-span-3">       
+            <ul className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
                 { !selectedCarType ? Object.entries(carData.carData).map(([carType, carInfo])=>(
                     <li key={carType}>
                         <a href="#" className="group block overflow-hidden">
-                        <img
-                            src={carInfo.image}
-                            alt=""
-                            className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
-                        />
+                            <img
+                                src={carInfo.image}
+                                alt=""
+                                className="h-[350px] w-full object-cover transition duration-500 group-hover:scale-105 sm:h-[450px]"
+                            />
 
-                        <div className="relative bg-white pt-3">
-                            <h3
-                            className="text-xl text-gray-700 group-hover:underline group-hover:underline-offset-4"
-                            >
-                            {carInfo.name} - {carType}
-                            </h3>
-                            <p className="mt-2">
-                            <span className="text-sm font-semibold">Parts:</span>
-                            <br />
-                            {carInfo.parts.map((part, index)=>(
-                                <span key={index}>
-                                    <span className="tracking-wider text-xs text-gray-900">{part}</span>
-                                    {index !== carInfo.parts.length - 1 && <span>, </span>}
-                                </span>
-                                
-                            ))
-
-                            }
-                            </p>
-                        </div>
+                            <div className="relative bg-white pt-3">
+                                <h3 className="text-xl text-gray-700 group-hover:underline group-hover:underline-offset-4">
+                                    {carInfo.name} - {carType}
+                                </h3>
+                                <p className="mt-2">
+                                    <span className="text-sm font-semibold">Parts:</span>
+                                    <br />
+                                    {carInfo.parts.map((part, index)=>(
+                                            <span key={index}>
+                                                <span className="tracking-wider text-xs text-gray-900">{part}</span>
+                                                {index !== carInfo.parts.length - 1 && <span>, </span>}
+                                            </span>
+                                        
+                                        ))
+                                    }
+                                </p>
+                            </div>
                         </a>
                     </li>
-
-
                 )) : (
                     <div>
                         <a href="#" className="group block overflow-hidden">
@@ -119,8 +108,8 @@ const ListCars = (carData) => {
 
                 }
                 
-                </ul>
-            </div>
+            </ul>
+        </div>
             </div>
         </div>
     </div>
